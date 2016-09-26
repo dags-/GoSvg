c1024a7ced5b727456fc44bb9ca260004ce77d62
dags- <dags@dags.me>
2016-09-26 14:33:48 +0100

    non-alpha and alpha

--- demo.go
+++ demo.go
@@ -3,16 +3,15 @@ package main
 import (
 	"os"
 	"image/png"
-	"github.com/gorilla/GoSvg/svg"
+	"github.com/dags-/gosvg/svg"
 	"net/http"
 )
 
 func main() {
-	url := "https://40.media.tumblr.com/0c57428045d4cb0d6ada4643c9913bc2/tumblr_inline_ny1oze2U7G1r70e84_540.png"
-	resp, err := http.Get(url)
+	resp, err := http.Get("https://40.media.tumblr.com/0c57428045d4cb0d6ada4643c9913bc2/tumblr_inline_ny1oze2U7G1r70e84_540.png")
 	if err == nil {
 		img, _ := png.Decode(resp.Body)
 		out, _ := os.Create("meow.svg")
-		svg.Convert(img, out)
+		svg.ConvertWithAlpha(img, out)
 	}
 }
--- svg/svg.go
+++ svg/svg.go
@@ -8,7 +8,8 @@ import (
 )
 
 const headerFormat = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 %v %v' shape-rendering='crispEdges'>"
-const pixelFormat = "<path d='M%v %vH%v' stroke='rgba(%v,%v,%v,%v)'/>"
+const noAlpha = "<path d='M%v %vH%v' stroke='rgb(%v,%v,%v)'/>"
+const withAlpha = "<path d='M%v %vH%v' stroke='rgba(%v,%v,%v,%v)'/>"
 
 func Convert(img image.Image, wr io.Writer) {
 	max := img.Bounds().Max
@@ -18,7 +19,21 @@ func Convert(img image.Image, wr io.Writer) {
 		for x := 0; x < max.X; x++ {
 			col := color.RGBAModel.Convert(img.At(x, y))
 			c := col.(color.RGBA)
-			fmt.Fprintf(wr, pixelFormat, x, yy, x + 1, c.R, c.G, c.B, c.A)
+			fmt.Fprintf(wr, noAlpha, x, yy, x + 1, c.R, c.G, c.B)
+		}
+	}
+	fmt.Fprint(wr, "</svg>")
+}
+
+func ConvertWithAlpha(img image.Image, wr io.Writer)  {
+	max := img.Bounds().Max
+	fmt.Fprintf(wr, headerFormat, max.X, max.Y)
+	for y := 0; y < max.Y; y++ {
+		yy := 0.5 + float32(y)
+		for x := 0; x < max.X; x++ {
+			col := color.RGBAModel.Convert(img.At(x, y))
+			c := col.(color.RGBA)
+			fmt.Fprintf(wr, withAlpha, x, yy, x + 1, c.R, c.G, c.B, c.A)
 		}
 	}
 	fmt.Fprint(wr, "</svg>")
